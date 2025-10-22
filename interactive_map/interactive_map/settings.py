af1be9cd5d7634a220a47fc80ece08e3b3383deb
Anastasia Kamysheva <anastasiia.kamysheva@outlook.com>
2025-10-22 12:03:39 +0400

    Add frontend

--- interactive_map/interactive_map/settings.py
+++ interactive_map/interactive_map/settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = "interactive_map.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "frontend"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -107,9 +107,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "frontend"]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- interactive_map/interactive_map/urls.py
+++ interactive_map/interactive_map/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-
-def main_page(request):
-    return HttpResponse("<h1>Здесь будет карта</h1>")
+from django.urls import path, re_path
+from django.conf import settings
+from django.views.static import serve
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path("", main_page, name="main_page"),
+    path("", TemplateView.as_view(template_name="index.html")),
     path("admin/", admin.site.urls),
+    re_path(
+        r"^(?P<path>.*)$", serve, {"document_root": settings.BASE_DIR / "frontend"}
+    ),
 ]
