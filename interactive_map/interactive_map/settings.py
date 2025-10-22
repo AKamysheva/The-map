12cda18b7cff41fe2ee03a36616bd08f96830261
Anastasia Kamysheva <anastasiia.kamysheva@outlook.com>
2025-10-22 14:24:04 +0400

    Registration application, add media root

--- interactive_map/interactive_map/settings.py
+++ interactive_map/interactive_map/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "app.apps.AppConfig",
 ]
 
 MIDDLEWARE = [
@@ -111,3 +112,7 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "frontend"]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
