ac0fc5a5d7f1e45678c8b6f41fd3786f3c3c118e
Anastasia Kamysheva <anastasiia.kamysheva@outlook.com>
2025-10-23 15:52:02 +0400

    Add dynamic image upload path for Place images

--- interactive_map/app/models.py
+++ interactive_map/app/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 
+def place_image_path(instance, filename):
+    return f"images/place_{instance.place.id}/{filename}"
+
+
 class Place(models.Model):
     title = models.CharField(max_length=30, verbose_name="Название места")
     description_short = models.TextField(verbose_name="Короткое описание")
@@ -12,4 +16,4 @@ class Place(models.Model):
 
 class PlaceImage(models.Model):
     place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name="imgs")
-    image = models.ImageField(upload_to="images/place/", verbose_name="Картинки места")
+    image = models.ImageField(upload_to=place_image_path, verbose_name="Картинки места")
